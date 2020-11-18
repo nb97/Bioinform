39febbd6b0c8927c47db32624a0e98cd26ec5707
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-11-17 19:45:35 -0800

    package development

--- R/sqrt_numeric.R
+++ R/sqrt_numeric.R
@@ -1,8 +1,23 @@
+#' Square root numeric values
+#'
+#' @param data Numeric intergers
+#' @param na.rm Removing NA;s
+#' @param verbose returnign a mesage
+#'
+#' @return
+#' A data frame with no NAs and all values squared.
+#' @export
+#'
+#' @examples
+#' sqrt_function <- function(data, na.rm = TRUE, verbose = TRUE) {
+#'if (verbose) message("Finding SQRT...")
+#'mutate(data, across(where(is.numeric), ~
+#'                      sqrt(.x))
+#')}
+
+#'tibble1 <- sqrt_function(gapminder)
+#'tibble2 <- sqrt_function(penguins)
 
 
+#Function:
 
-sqrt_function <- function(data, na.rm = TRUE, verbose = TRUE) {
-  if (verbose) message("Finding SQRT...")
-  mutate(data, across(where(is.numeric), ~
-                        sqrt(.x))
-  )}
