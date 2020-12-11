7579222cc112770587fb09116a5c54310aacd536
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2020-12-10 19:23:00 -0800

    changes in readme

--- tests/testthat/cq_value-test.R
+++ tests/testthat/cq_value-test.R
@@ -2,4 +2,5 @@ test_that("cq value test", {
   expect_equal(Cq_value(5), 6.221)
 })
 
+#This test shows that the expected value (6.221) by manually solving for Cq with x=5 would equal the same as usign Cq_value function.
 
